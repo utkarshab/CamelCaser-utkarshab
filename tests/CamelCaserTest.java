4a48f80bd80c53e20217ca66e1a45543d73823ea
utkarshab <ub3@illinois.edu>
2018-01-23 16:33:18 -0600

    Testing Exceptions

--- tests/CamelCaserTest.java
+++ tests/CamelCaserTest.java
@@ -3,9 +3,42 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CamelCaserTest {
-
     @Test
     public void handoutTestCamelCase() {
         assertEquals("thisIsMyString", CamelCaser.camelCase("this is my string"));
     }
+
+
+    @Test
+    public void testInvalidCharacter() {
+    try {
+        CamelCaser.camelCase("how @re you?");
+        fail("Expected an IllegalArgumentException to be Thrown");
+    } catch (IllegalArgumentException e) {
+        assertEquals("Invalid Character", e.getMessage());
+    }
+}
+    @Test
+    public void testInvalidFormat() {
+        try {
+            CamelCaser.camelCase("2 nice 4 u");
+            fail("Expected an IllegalArgumentException to be Thrown");
+        }
+        catch (IllegalArgumentException e){
+            assertEquals("Invalid Format", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testNullInput() {
+        try {
+            CamelCaser.camelCase(null);
+            fail("Expected an IllegalArgumentException to be Thrown");
+        }
+        catch (IllegalArgumentException e){
+            assertEquals("Invalid Format", e.getMessage());
+        }
+    }
+
+
 }
